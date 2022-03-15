da4e9397782f4361e5ddce3888055e37d64e8ef5
설혜린 <tosiltosilkk@gmail.com>
2022-03-15 16:16:01 +0900

    스크롤 애니메이션 구현

--- js/main.js
+++ js/main.js
@@ -11,6 +11,14 @@
       scrollHeight: 0,
       objs: {
         container: document.querySelector("#scroll-section-0"),
+        messageA: document.querySelector("#scroll-section-0 .main-message.a"),
+        messageB: document.querySelector("#scroll-section-0 .main-message.b"),
+        messageC: document.querySelector("#scroll-section-0 .main-message.c"),
+        messageD: document.querySelector("#scroll-section-0 .main-message.d"),
+      },
+
+      values: {
+        messageA_opacity: [0, 1],
       },
     },
     {
@@ -50,28 +58,69 @@
         i
       ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
-    console.log(sceneInfo);
+
+    let yOffset = window.pageYOffset;
+    let totalScrollHeight = 0;
+
+    for (let i = 0; i < sceneInfo.length; i++) {
+      totalScrollHeight += sceneInfo[i].scrollHeight;
+      if (totalScrollHeight >= yOffset) {
+        currentScene = i;
+        break;
+      }
+    }
+    document.body.setAttribute("id", `show-scene-${currentScene}`);
+  }
+
+  function calcValues(values, currentYOffset) {}
+
+  function playAnimation() {
+    const objs = sceneInfo[currentScene].objs;
+    const values = sceneInfo[currentScene].values;
+    const currentYOffset = yOffset - prevScrollHeight;
+
+    switch (currentScene) {
+      case 0:
+        let messageA_opacity_0 = values.messageA_opacity[0];
+        let messageA_opacity_1 = values.messageA_opacity[1];
+        console.log(calcValues(values.messageA_opacity, currentYOffset));
+
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+    }
   }
 
   function scrollLoop() {
     prevScrollHeight = 0;
+
     for (let i = 0; i < currentScene; i++) {
       prevScrollHeight += sceneInfo[i].scrollHeight;
     }
+
     if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
       currentScene++;
+      document.body.setAttribute("id", `show-scene-${currentScene}`);
     }
+
     if (yOffset < prevScrollHeight) {
       if (currentScene == 0) return; //브라우저 바운스 효과로 인해 마이너스가 되는 것 방지(모바일)
       currentScene--;
+      document.body.setAttribute("id", `show-scene-${currentScene}`);
     }
-    console.log(currentScene);
+
+    playAnimation();
   }
 
-  window.addEventListener("resize", setLayout);
   window.addEventListener("scroll", () => {
     yOffset = window.pageYOffset;
     scrollLoop();
   });
-  setLayout();
+
+  window.addEventListener("load", setLayout);
+  window.addEventListener("resize", setLayout);
 })();
